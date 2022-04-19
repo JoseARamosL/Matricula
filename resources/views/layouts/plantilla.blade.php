0a0919de827e71558f7ef82a3ef743ed571b7f83
JoseARamosL <j.ramoslado2001@gmail.com>
2022-04-19 11:01:13 +0200

    Matricula4

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -26,10 +26,10 @@
     @include('layouts.footer')
 </div>
  <!-- Chatbot ALMA -->
- <script id="helper" data-idEmpresa="11b575c0-3b73-11ea-9b14-afbcefb5bb52" data-idChatbot="91401df0-a368-11ec-9289-add28a26e8a2" data-install="1" src="https://assistant.almaintelligence.com:7777/stylesAndScripts/version3/iframe.js"></script>
+ {{-- <script id="helper" data-idEmpresa="11b575c0-3b73-11ea-9b14-afbcefb5bb52" data-idChatbot="91401df0-a368-11ec-9289-add28a26e8a2" data-install="1" src="https://assistant.almaintelligence.com:7777/stylesAndScripts/version3/iframe.js"></script> --}}
  <!-- Fin Chatbot ALMA -->
  <script>
-     $('#imgAlma').attr('src', '{{ URL::to('/') }}/images/svg/bubble_2.svg');
+    //  $('#imgAlma').attr('src', '{{ URL::to('/') }}/images/svg/bubble_2.svg');
  </script>
 </body>
 </html>
